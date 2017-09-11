fa1331b3fdcafc327c8a4e26d443041d063bac19
jeffswitzer <jeff.switzer@gmail.com>
2017-09-11 13:48:10 -0700

    moving cache into query param, and testing with timeseries

--- api/controllers/six.js
+++ api/controllers/six.js
@@ -6,23 +6,30 @@ function areaStats(req, res) {
     let date = req.swagger.params['date'].value;
     let plant = req.swagger.params['plant'].value;
     let climate = req.swagger.params['climate'].value;
+    let useCache = req.swagger.params['useCache'].value;
 
-    return db.getSixAreaStats(boundary, date, plant, phenophase, climate)
-        .then((areaStatsResponse) => res.status(200).send(areaStatsResponse))
-        .catch((error) => res.status(500).json({"message": error.message}));
+    if (useCache) {
+        return db.getSixAreaStatsWithCaching(boundary, date, plant, phenophase, climate)
+            .then((areaStatsResponse) => res.status(200).send(areaStatsResponse))
+            .catch((error) => res.status(500).json({"message": error.message}));
+    } else {
+        return db.getSixAreaStats(boundary, date, plant, phenophase, climate)
+            .then((areaStatsResponse) => res.status(200).send(areaStatsResponse))
+            .catch((error) => res.status(500).json({"message": error.message}));
+    }
 }
 
-function areaStatsWithCaching(req, res) {
-    let boundary = req.swagger.params['boundary'].value;
-    let phenophase = req.swagger.params['phenophase'].value;
-    let date = req.swagger.params['date'].value;
-    let plant = req.swagger.params['plant'].value;
-    let climate = req.swagger.params['climate'].value;
-
-    return db.getSixAreaStatsWithCaching(boundary, date, plant, phenophase, climate)
-        .then((areaStatsResponse) => res.status(200).send(areaStatsResponse))
-        .catch((error) => res.status(500).json({"message": error.message}));
-}
+// function areaStatsWithCaching(req, res) {
+//     let boundary = req.swagger.params['boundary'].value;
+//     let phenophase = req.swagger.params['phenophase'].value;
+//     let date = req.swagger.params['date'].value;
+//     let plant = req.swagger.params['plant'].value;
+//     let climate = req.swagger.params['climate'].value;
+//
+//     return db.getSixAreaStatsWithCaching(boundary, date, plant, phenophase, climate)
+//         .then((areaStatsResponse) => res.status(200).send(areaStatsResponse))
+//         .catch((error) => res.status(500).json({"message": error.message}));
+// }
 
 async function areaStatsTimeSeries(req, res) {
     let boundary = req.swagger.params['boundary'].value;
@@ -31,12 +38,18 @@ async function areaStatsTimeSeries(req, res) {
     let endYear = req.swagger.params['yearEnd'].value;
     let plant = req.swagger.params['plant'].value;
     let climate = req.swagger.params['climate'].value;
+    let useCache = req.swagger.params['useCache'].value;
 
     let yearRange = [...Array(endYear - startYear + 1).keys()].map(i => startYear + i);
 
     try {
         let promiseResults = await Promise.all(yearRange.map(async (year) => {
-            let resultForYear = await db.getSixAreaStats(boundary, new Date(year, 0, 1), plant, phenophase, climate);
+            let resultForYear;
+            if (useCache) {
+                resultForYear = await db.getSixAreaStatsWithCaching(boundary, new Date(year, 0, 1), plant, phenophase, climate);
+            } else {
+                resultForYear = await db.getSixAreaStats(boundary, new Date(year, 0, 1), plant, phenophase, climate);
+            }
             return resultForYear;
         }));
         return res.status(200).send({timeSeries: promiseResults});
@@ -46,5 +59,5 @@ async function areaStatsTimeSeries(req, res) {
 }
 
 module.exports.areaStats = areaStats;
-module.exports.areaStatsWithCaching = areaStatsWithCaching;
+// module.exports.areaStatsWithCaching = areaStatsWithCaching;
 module.exports.areaStatsTimeSeries = areaStatsTimeSeries;
