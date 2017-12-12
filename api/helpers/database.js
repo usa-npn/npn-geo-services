d52fa431fef52b5556f4705bdb5eb08cd220c287
jeffswitzer <jeff.switzer@gmail.com>
2017-12-12 09:58:04 -0700

    dynamically generate swagger yaml file

--- api/helpers/database.js
+++ api/helpers/database.js
@@ -3,77 +3,77 @@ const { Pool, Client } = require('pg');
 const pgPool = new Pool();
 
 // mysql ppols
-var mysql = require('mysql2');
-var mysqlPromise = require('mysql2/promise');
+// var mysql = require('mysql2');
+// var mysqlPromise = require('mysql2/promise');
+//
+// let usanpnHost = '';
+// let usanpnUser = '';
+// let usanpnPassword = '';
+// let usanpnDatabase = '';
+//
+// let drupalHost = '';
+// let drupalUser = '';
+// let drupalPassword = '';
+// let drupalDatabase = '';
 
-let usanpnHost = '';
-let usanpnUser = '';
-let usanpnPassword = '';
-let usanpnDatabase = '';
+// if (process.env.NODE_ENV === 'production') {
+//     usanpnHost = process.env.OPS_USANPN_HOST;
+//     usanpnUser = process.env.OPS_USANPN_USER;
+//     usanpnPassword = process.env.OPS_USANPN_PASSWORD;
+//     usanpnDatabase = process.env.OPS_USANPN_DATABASE;
+//
+//     drupalHost = process.env.OPS_DRUPAL_HOST;
+//     drupalUser = process.env.OPS_DRUPAL_USER;
+//     drupalPassword = process.env.OPS_DRUPAL_PASSWORD;
+//     drupalDatabase = process.env.OPS_DRUPAL_DATABASE;
+// } else if (process.env.NODE_ENV === 'development') {
+//     usanpnHost = process.env.DEV_USANPN_HOST;
+//     usanpnUser = process.env.DEV_USANPN_USER;
+//     usanpnPassword = process.env.DEV_USANPN_PASSWORD;
+//     usanpnDatabase = process.env.DEV_USANPN_DATABASE;
+//
+//     drupalHost = process.env.DEV_DRUPAL_HOST;
+//     drupalUser = process.env.DEV_DRUPAL_USER;
+//     drupalPassword = process.env.DEV_DRUPAL_PASSWORD;
+//     drupalDatabase = process.env.DEV_DRUPAL_DATABASE;
+// } else {
+//     usanpnHost = process.env.LOCAL_USANPN_HOST;
+//     usanpnUser = process.env.LOCAL_USANPN_USER;
+//     usanpnPassword = process.env.LOCAL_USANPN_PASSWORD;
+//     usanpnDatabase = process.env.LOCAL_USANPN_DATABASE;
+//
+//     drupalHost = process.env.LOCAL_DRUPAL_HOST;
+//     drupalUser = process.env.LOCAL_DRUPAL_USER;
+//     drupalPassword = process.env.LOCAL_DRUPAL_PASSWORD;
+//     drupalDatabase = process.env.LOCAL_DRUPAL_DATABASE;
+// }
+//
+// var usanpnPool = mysql.createPool({
+//     connectionLimit : 20,
+//     host     : usanpnHost,
+//     user     : usanpnUser,
+//     password : usanpnPassword,
+//     database : usanpnDatabase,
+//     debug    :  false
+// });
+// var usanpnPromisePool = mysqlPromise.createPool({
+//     connectionLimit : 20,
+//     host     : usanpnHost,
+//     user     : usanpnUser,
+//     password : usanpnPassword,
+//     database : usanpnDatabase,
+//     debug    :  false
+// });
+// var drupalPool = mysql.createPool({
+//     connectionLimit : 20,
+//     host     : drupalHost,
+//     user     : drupalUser,
+//     password : drupalPassword,
+//     database : drupalDatabase,
+//     debug    :  false
+// });
 
-let drupalHost = '';
-let drupalUser = '';
-let drupalPassword = '';
-let drupalDatabase = '';
-
-if (process.env.NODE_ENV === 'production') {
-    usanpnHost = process.env.OPS_USANPN_HOST;
-    usanpnUser = process.env.OPS_USANPN_USER;
-    usanpnPassword = process.env.OPS_USANPN_PASSWORD;
-    usanpnDatabase = process.env.OPS_USANPN_DATABASE;
-
-    drupalHost = process.env.OPS_DRUPAL_HOST;
-    drupalUser = process.env.OPS_DRUPAL_USER;
-    drupalPassword = process.env.OPS_DRUPAL_PASSWORD;
-    drupalDatabase = process.env.OPS_DRUPAL_DATABASE;
-} else if (process.env.NODE_ENV === 'development') {
-    usanpnHost = process.env.DEV_USANPN_HOST;
-    usanpnUser = process.env.DEV_USANPN_USER;
-    usanpnPassword = process.env.DEV_USANPN_PASSWORD;
-    usanpnDatabase = process.env.DEV_USANPN_DATABASE;
-
-    drupalHost = process.env.DEV_DRUPAL_HOST;
-    drupalUser = process.env.DEV_DRUPAL_USER;
-    drupalPassword = process.env.DEV_DRUPAL_PASSWORD;
-    drupalDatabase = process.env.DEV_DRUPAL_DATABASE;
-} else {
-    usanpnHost = process.env.LOCAL_USANPN_HOST;
-    usanpnUser = process.env.LOCAL_USANPN_USER;
-    usanpnPassword = process.env.LOCAL_USANPN_PASSWORD;
-    usanpnDatabase = process.env.LOCAL_USANPN_DATABASE;
-
-    drupalHost = process.env.LOCAL_DRUPAL_HOST;
-    drupalUser = process.env.LOCAL_DRUPAL_USER;
-    drupalPassword = process.env.LOCAL_DRUPAL_PASSWORD;
-    drupalDatabase = process.env.LOCAL_DRUPAL_DATABASE;
-}
-
-var usanpnPool = mysql.createPool({
-    connectionLimit : 20,
-    host     : usanpnHost,
-    user     : usanpnUser,
-    password : usanpnPassword,
-    database : usanpnDatabase,
-    debug    :  false
-});
-var usanpnPromisePool = mysqlPromise.createPool({
-    connectionLimit : 20,
-    host     : usanpnHost,
-    user     : usanpnUser,
-    password : usanpnPassword,
-    database : usanpnDatabase,
-    debug    :  false
-});
-var drupalPool = mysql.createPool({
-    connectionLimit : 20,
-    host     : drupalHost,
-    user     : drupalUser,
-    password : drupalPassword,
-    database : drupalDatabase,
-    debug    :  false
-});
-
-module.exports.usanpnPool = usanpnPool;
-module.exports.usanpnPromisePool = usanpnPromisePool;
-module.exports.drupalPool = drupalPool;
+// module.exports.usanpnPool = usanpnPool;
+// module.exports.usanpnPromisePool = usanpnPromisePool;
+// module.exports.drupalPool = drupalPool;
 module.exports.pgPool = pgPool;
--- app.js
+++ app.js
@@ -6,64 +6,72 @@ var fs = require('fs');
 var path = require("path");
 var morgan = require('morgan');
 let log = require('./logger.js');
+let generateSwagger = require('./generateSwagger.js');
 
-var app = express();
+generateSwagger.generate().then(() => {
 
-// create a write stream (in append mode) and set up a log to record requests
-let accessLogStream = fs.createWriteStream(path.join("./logs", "access.log"), {flags: "a"});
-app.use(morgan("combined", {stream: accessLogStream}));
+    var app = express();
 
+    // create a write stream (in append mode) and set up a log to record requests
+    let accessLogStream = fs.createWriteStream(path.join("./logs", "access.log"), {flags: "a"});
+    app.use(morgan("combined", {stream: accessLogStream}));
 
+    module.exports = app; // for testing
+    const util = require('util');
 
-module.exports = app; // for testing
-const util = require('util');
+    var config = {
+        appRoot: __dirname, // required config
+        swaggerSecurityHandlers: {
+            api_key: function (req, authOrSecDef, scopesOrApiKey, cb) {
+                // your security code
+                if ('1234' === scopesOrApiKey) {
+                    cb(null);
+                } else {
+                    cb(new Error('access denied!'));
+                }
+            }
+        }
+    };
 
-var config = {
-  appRoot: __dirname, // required config
-  swaggerSecurityHandlers: {
-    api_key: function (req, authOrSecDef, scopesOrApiKey, cb) {
-      // your security code
-      if ('1234' === scopesOrApiKey) {
-        cb(null);
-      } else {
-        cb(new Error('access denied!'));
-      }
-    }
-  }
-};
+    process.on("uncaughtException", (err) => {
+        log.error(err, "Something Broke!.");
+        console.error(err.stack);
+    });
 
-process.on("uncaughtException", (err) => {
-    log.error(err, "Something Broke!.");
-    console.error(err.stack);
-});
+    app.use((req,res,next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
-app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+    app.use(express.static('api/swagger'));
 
-app.use(express.static('api/swagger'));
-
-const imagePath = '/var/www/data-site/files/npn-geo-services/clipped_images';
+    const imagePath = '/var/www/data-site/files/npn-geo-services/clipped_images';
 
 // app.use(express.static('static/rasters'));
-app.use(express.static(imagePath));
+    app.use(express.static(imagePath));
 
-SwaggerExpress.create(config, (err, swaggerExpress) => {
-  if (err) { throw err; }
+    SwaggerExpress.create(config, (err, swaggerExpress) => {
+        if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+        // install middleware
+        swaggerExpress.register(app);
 
-  //todo 3006 is here because 'swagger project test' doesn't pick up the env port
-  app.listen(process.env.PORT || 3006);
+        //todo 3006 is here because 'swagger project test' doesn't pick up the env port
+        app.listen(process.env.PORT || 3006);
 
-  log.info('listening on port 3006');
-  log.error('this is a test error!');
+        log.info('listening on port 3006');
+        log.error('this is a test error!');
 
-  setInterval(function() {
-    console.log('HeapUsed in MB: ' + process.memoryUsage().heapUsed / 1048576);
-    //console.log(util.inspect(process.memoryUsage()));
-  },1000);
+        setInterval(function() {
+            console.log('HeapUsed in MB: ' + process.memoryUsage().heapUsed / 1048576);
+            //console.log(util.inspect(process.memoryUsage()));
+        },1000);
+    });
+
+}).catch(err => {
+    log.error(err, 'could not generate swagger.yaml, haulting server');
+    process.exit(1);
 });
+
+
