8d08d9a60152cecdce868b0c0d2ff74411e66e08
jeffswitzer <jeff.switzer@gmail.com>
2018-02-07 14:46:36 -0700

    trying emerald ash borer solid sld

--- updatePestMapCache.js
+++ updatePestMapCache.js
@@ -37,26 +37,29 @@ function doRequest(path) {
 
 async function update() {
 
-    let species = 'Emerald Ash Borer';
+    let speciesArr = ['Emerald Ash Borer', 'Apple Maggot'];
     let aprilStartDate = false;
 
-    var start = moment.utc("2017-01-01");
-    var end = moment.utc("2017-12-31");
+    for(var species of speciesArr) {
+        var start = moment.utc("2018-01-01");
+        var end = moment.utc("2018-02-06");
 
-    while(start <= end){
+        while(start <= end){
 
-        let dateString = start.format('YYYY-MM-DD');
+            let dateString = start.format('YYYY-MM-DD');
 
-        try {
-            console.log(`generating pest map: ${species} ${dateString}`);
-            await doRequest(`/v0/agdd/pestMap?species=${species}&date=${dateString}&aprilStartDate=${aprilStartDate}`);
-            //await agddController.getPestMap(species, start, aprilStartDate);
-        } catch(error) {
-            console.log(error);
+            try {
+                console.log(`generating pest map: ${species} ${dateString}`);
+                await doRequest(`/v0/agdd/pestMap?species=${species}&date=${dateString}&aprilStartDate=${aprilStartDate}`);
+                //await agddController.getPestMap(species, start, aprilStartDate);
+            } catch(error) {
+                console.log(error);
+            }
+
+            start.add(1, 'days');
         }
-
-        start.add(1, 'days');
     }
+
 }
 
 update();
