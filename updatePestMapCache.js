d682efb65608c13704a68fddcd1cdb028e93ecb5
jeffswitzer <jeff.switzer@gmail.com>
2018-02-22 14:44:15 -0700

    cleanup update pest cached images

--- updatePestMapCache.js
+++ updatePestMapCache.js
@@ -1,87 +1,67 @@
 const moment = require('moment');
 const https = require('https');
-var fs = require('fs');
+const fs = require('fs');
+const os = require('os');
+let log = require('./logger.js');
 
+// hits the geo services pestMap endpoint to generate image
 function doRequest(path) {
-    // let options = {
-    //     "strictSSL": false,
-    //     "rejectUnauthorized": false,
-    //     "hostname": "data-dev.usanpn.org",
-    //     "path": encodeURI(path),
-    //     "method": "GET",
-    //     "port": 3006
-    // };
     let options = {
-        hostname: 'data-dev.usanpn.org',
+        hostname: (os.hostname() == 'npnweb-dev.npn.edu') ? 'data-dev.usanpn.org' : 'data.usanpn.org',
         port: 3006,
         path: encodeURI(path),
         method: 'GET',
         rejectUnauthorized: false
     };
     return new Promise ((resolve, reject) => {
-        // 'https://data-dev.usanpn.org:3006/v0/agdd/pestMap?species=Emerald%20Ash%20Borer&date=2017-01-05&aprilStartDate=false'
+        // 'https://data-dev.usanpn.org:3006/v0/agdd/pestMap?species=Emerald%20Ash%20Borer&date=2017-01-05'
         https.get(options, (response) => {
             resolve(response);
         }).on('error', (e) => {
             reject(e);
         });
-
-        // req.on('response', res => {
-        //     resolve(res);
-        // });
-        //
-        // req.on('error', err => {
-        //     reject(err);
-        // });
     });
 }
 
+// deletes all pest images for species from past 5 days through next 6 days
 async function deleteForecastDays(species) {
-    var start = moment.utc().subtract(2,'days');
-    var end = moment.utc().add(5,'days');
-
+    let start = moment.utc().subtract(5,'days');
+    let end = moment.utc().add(6,'days');
     let pestImagePath = '/var/www/data-site/files/npn-geo-services/clipped_images/pest_maps/';
     while(start <= end) {
         let dateString = start.format('YYYY-MM-DD');
         let fileName = `${species.replace(/\s/g, '_')}_${dateString}_styled.png`;
-
         if (fs.existsSync(pestImagePath + fileName)) {
             console.log(`deleting file for regineration: ${pestImagePath + fileName}`);
             fs.unlinkSync(pestImagePath + fileName);
         }
-
         start.add(1, 'days');
     }
 }
 
 async function update() {
-
-    let speciesArr = ['Emerald Ash Borer', 'Apple Maggot', 'Hemlock Woolly Adelgid', 'Lilac Borer', 'Winter Moth'];
-    let aprilStartDate = false;
-
-    for(var species of speciesArr) {
-
-        //await deleteForecastDays(species);
-
-        var start = moment.utc("2017-01-01");
-        var end = moment.utc().add(5,'days');
-
-        while(start <= end){
-
-            let dateString = start.format('YYYY-MM-DD');
-
-            try {
-                console.log(`generating pest map: ${species} ${dateString}`);
-                await doRequest(`/v0/agdd/pestMap?species=${species}&date=${dateString}&aprilStartDate=${aprilStartDate}`);
-                //await agddController.getPestMap(species, start, aprilStartDate);
-            } catch(error) {
-                console.log(error);
+    try {
+        log.info("updating cached pest images.");
+        let speciesArr = ['Emerald Ash Borer', 'Apple Maggot', 'Hemlock Woolly Adelgid', 'Lilac Borer', 'Winter Moth'];
+        for(var species of speciesArr) {
+            await deleteForecastDays(species);
+            let start = moment.utc("2017-01-01");
+            let end = moment.utc().add(6,'days');
+            while(start <= end){
+                let dateString = start.format('YYYY-MM-DD');
+                try {
+                    console.log(`generating pest map: ${species} ${dateString}`);
+                    await doRequest(`/v0/agdd/pestMap?species=${species}&date=${dateString}`);
+                } catch(error) {
+                    console.log(error);
+                }
+                start.add(1, 'days');
             }
-
-            start.add(1, 'days');
         }
+        log.info("finished updating cached pest images.");
+    } catch (error) {
+        log.error("could not update pest cached images: " + error);
     }
-
 }
 
 update();
