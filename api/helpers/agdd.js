c0fb693e96305efb823e1cb175a8930ae56a0bca
Jeff Switzer <jeff.switzer@gmail.com>
2019-01-28 10:18:44 -0700

    fix range shape file path

--- api/helpers/agdd.js
+++ api/helpers/agdd.js
@@ -355,7 +355,7 @@ async function getPestMap(species, date, preserveExtent) {
         //https://geoserver-dev.usanpn.org/geoserver/gdd/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gdd:states&CQL_FILTER=NAME IN ('Arizona', 'Texas')&outputFormat=SHAPE-ZIP
         //let shpQuery = `https://geoserver-dev.usanpn.org/geoserver/gdd/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gdd:states&CQL_FILTER=NAME IN (${stateNames.join()})&outputFormat=SHAPE-ZIP`;
         //https://geoserver-dev.usanpn.org/geoserver/gdd/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gdd:states&CQL_FILTER=NAME IN ('Maine','Vermont','Colorado','Nebraska','Kansas','Oklahoma','Texas','Minnesota','Iowa','Missouri','Arkansas','Louisiana','Wisconsin','Illinois','Kentucky','Tennessee','Mississippi','Michigan','Indiana','Alabama','Ohio','Alabama','Georgia','South Carolina','North Carolina','Virginia','West Virginia','District of Columbia','Maryland','Delaware','New Jersey','Pennsylvania','New York','Connecticut','Rhode Island','Massachusetts','New Hampshire','Florida')&outputFormat=SHAPE-ZIP
-        let shapefile = `/var/www/data-site/files/npn-geo-services/shape_files/${pest.species.replace(/ /g, '_').toLowerCase()}_range/states.shp`;
+        let shapefile = pest.rangeShpFilePath;//`/var/www/data-site/files/npn-geo-services/shape_files/${pest.species.replace(/ /g, '_').toLowerCase()}_range/states.shp`;
         // when not preserving extent, use -te to set bounds to the shapefile bounding box
         let clipCommand = `gdalwarp -srcnodata -9999 -dstnodata -9999 -te ${pest.bounds.join(' ')} -overwrite -cutline ${shapefile} ${pestImagePath}${tiffFileName} ${pestImagePath}${croppedPngFilename}`;
         if(preserveExtent) {
@@ -368,8 +368,8 @@ async function getPestMap(species, date, preserveExtent) {
     }
 
     // // remove the uncropped tiff
-    // log.info(`removing: ` + pestImagePath + tiffFileName);
-    // await helpers.unlinkPromise(pestImagePath + tiffFileName);
+    log.info(`removing: ` + pestImagePath + tiffFileName);
+    await helpers.unlinkPromise(pestImagePath + tiffFileName);
     
     // style the tiff into png
     try {
