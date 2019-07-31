73fdc2353e75c2cd8d47d808cc02b0515fff9306
Jeff Switzer <jeff.switzer@gmail.com>
2019-07-31 13:32:24 -0700

    undo prev commit

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ generateSwagger.generate().then(() => {
     });
 
     app.use((req,res,next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
 
