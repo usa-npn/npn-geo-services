acfe0b30b7f9ada6ed0ff7c06e84e3620c63493c
jeffswitzer <jeff.switzer@gmail.com>
2022-10-12 13:50:44 -0700

    port to geo-services pestmap

--- api/helpers/general.js
+++ api/helpers/general.js
@@ -75,7 +75,7 @@ function stylizeFile(filename, rasterpath, fileFormat, layerName){
     return new Promise((resolve, reject) =>
     {
         log.info(`styling ${rasterpath}${filename}`);
-        let unstyledFileRef = `${process.env.PROTOCOL}://${process.env.SERVICES_HOST}:${process.env.GEO_SERVICES_PORT}/${filename}`;
+        let unstyledFileRef = `${process.env.PROTOCOL}://${process.env.SERVICES_HOST}/geo-services/${filename}`;
         // if (process.env.PROTOCOL === 'https') {
         //     unstyledFileRef = `http://www.usanpn.org/files/gridded/cliped_images/${filename}`;
         // } else {
@@ -180,7 +180,7 @@ function stylizePestMap(filename, rasterpath, fileFormat, sldName, backgroundCol
     return new Promise((resolve, reject) =>
     {
         log.info(`styling ${rasterpath}${filename}`);
-        let unstyledFileRef = `${process.env.PROTOCOL}://${process.env.SERVICES_HOST}:${process.env.GEO_SERVICES_PORT}/pest_maps/${filename}`;
+        let unstyledFileRef = `${process.env.PROTOCOL}://${process.env.SERVICES_HOST}/geo-services/pest_maps/${filename}`;
 
         var postData = `
 <wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/2.0" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
